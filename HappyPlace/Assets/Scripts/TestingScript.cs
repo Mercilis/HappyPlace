70e405cd7fb991ed8605f76e22132dec323b267d
Dakota <dakotajones.red@gmail.com>
2018-10-08 18:05:14 -0600

    Created testing script for spawning objects from asset bundle

---
+++ HappyPlace/Assets/Scripts/TestingScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "realisticforest"));
+        if (myLoadedAssetBundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
+        GameObject prefab = myLoadedAssetBundle.LoadAsset<GameObject>("SM_Tree_Pine_01");
+        prefab.AddComponent<BoxCollider>();
+        BoxCollider boxCollider= prefab.GetComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+        MeshRenderer renderer = prefab.GetComponentInChildren<MeshRenderer>();
+        boxCollider.center = renderer.bounds.center;
+        boxCollider.size = renderer.bounds.size;
+        prefab.AddComponent<PlaceableObject>();
+        Instantiate(prefab);
+    }
+}
